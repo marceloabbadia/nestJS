0abe564e8a2a148ae0670afc93e5184183f39485
Marcelo Abbadia <marceloabbadia@Marcelos-MacBook-Pro.local>
2023-12-15 20:00:50 -0300

    Feat: Table Tags creation through TypeORM migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm";
 import { dataSourceOptions } from "./database.module"
 import { CreateCourseTable1702671740999 } from "src/migrations/1702671740999-CreateCourseTable";
+import { CreateTagsTable1702680744057 } from "src/migrations/1702680744057-CreateTagsTable";
 
 export const dataSource = new DataSource({
     ...dataSourceOptions,
     synchronize: false,
-    migrations: [CreateCourseTable1702671740999],
+    migrations: [CreateCourseTable1702671740999, CreateTagsTable1702680744057],
 })
