1a33f3d56b998674d2880be114a859de8c664ca3
Marcelo Abbadia <marceloabbadia@Marcelos-MacBook-Pro.local>
2023-12-15 22:12:57 -0300

    Feat: Created the 'CoursesId' column in the 'Courses_Tags' table through migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,9 +3,10 @@ import { dataSourceOptions } from "./database.module"
 import { CreateCourseTable1702671740999 } from "src/migrations/1702671740999-CreateCourseTable";
 import { CreateTagsTable1702680744057 } from "src/migrations/1702680744057-CreateTagsTable";
 import { CreateCoursesTagsTable1702684070389 } from "src/migrations/1702684070389-CreateCoursesTagsTable";
+import { AddCoursesIdToCoursesTagsTable1702687841729 } from "src/migrations/1702687841729-AddCoursesIdToCoursesTagsTable";
 
 export const dataSource = new DataSource({
     ...dataSourceOptions,
     synchronize: false,
-    migrations: [CreateCourseTable1702671740999, CreateTagsTable1702680744057, CreateCoursesTagsTable1702684070389],
+    migrations: [CreateCourseTable1702671740999, CreateTagsTable1702680744057, CreateCoursesTagsTable1702684070389, AddCoursesIdToCoursesTagsTable1702687841729],
 })
